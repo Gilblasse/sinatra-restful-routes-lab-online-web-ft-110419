595fb6433bd9ddbd2f74478e00328eab07471a97
Nethelbert <gilblasse@gmail.com>
2019-12-22 12:13:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,8 @@ class ApplicationController < Sinatra::Base
   end 
   
   post "/recipes" do 
-    Recipe.create(params)
-    redirect "/recipes/#{params[:id]}"
+    recipe = Recipe.create(params)
+    redirect "/recipes/#{recipe.id}"
   end 
   
   get '/recipes/:id' do 
